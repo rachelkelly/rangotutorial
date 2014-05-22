d9d28ebf00684f4db7ead24bf238836400a758d3
Rachel Kelly <rkellyalso@gmail.com>
2014-05-22 12:56:31 -0700

    buncha changes!  1/2way thru p5 of tut

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,10 +13,8 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^rango/', include('rango.urls')), # ADD THIS NEW TUPLE!
+    url(r'^rango/', include('rango.urls')), 
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
