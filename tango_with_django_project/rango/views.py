c349ab161a2fd8b53bf08a69d6b4a30f4388ea0d
Rachel Kelly <rkellyalso@gmail.com>
2014-05-30 17:52:05 -0700

    up to 6.4ish

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 
 # per request of 6.2.1, imports the Category model
 from rango.models import Category
+
+# per request of 6.3.3, imports Page model
 from rango.models import Page
 
 def index(request):
@@ -17,8 +19,14 @@ def index(request):
     # Place the list in our context_dict dictionary which will be passed to
     # the template engine
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categorys': category_list}
-    print context_dict
+    #context_dict = {'categorys': category_list}
+    context_dict = {'categories': category_list}
+    
+    # The following two lines are new.
+    # We loop through each category returned, and create a URL attribute.
+    # This attribute stores an encoded URL (e.g. spaces replaced w/ _).
+    for category in category_list:
+        category.url = category.name.replace(' ', '_')
 
     # Return a rendered response to send to the client.
     return render_to_response('rango/index.html', context_dict, context)
